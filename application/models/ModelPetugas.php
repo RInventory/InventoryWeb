6a58f921667e25d9da3de9de2afc8df7cf8a759b
DESKTOP-PG9QR23\user <dnovtahaning.ifu@gmail.com>
2018-05-11 10:29:28 +0700

    edit

--- application/models/ModelPetugas.php
+++ application/models/ModelPetugas.php
@@ -1,13 +1,31 @@
 <?php
 	class ModelPetugas extends CI_Model{
 
-		function list_petugas(){
+		public function list_petugas(){
 			return $this->db->get('login');
 		}
 
 		public function delete($username){
-        $this->db->where('username',$username);
-		$this->db->delete('login');
+	        $this->db->where('username',$username);
+			$this->db->delete('login');
+    	}
+
+    	public function edit_petugas($username){
+    		$this->db->where('username',$username);
+	 		$query = $this->db->get('login');
+			 	if($query->num_rows() > 0)
+			 	{
+			 		$data = $query->row();
+					$query->free_result(); 
+			 	} else{
+			 		$data = NULL;
+			 	}
+			 	return $data;
+    	}
+
+    	public function update($username,$data){
+    		$this->db->where('username',$username);
+			$this->db->update('login',$data);
     	}
 	}
 ?>
--- application/views/petugas/update.php
+++ application/views/petugas/update.php
@@ -1,28 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Demo CRUD</title>
-</head>
-<body>
-	<h2>Demo CRUD</h2>
-	<p><strong>Tambah Data</strong></p>
-
-	<form action="create" method="post">
-		<?php echo $model->label['kode']; ?><br>
-		<input type="text" name="kode" size="10" maxlength="10" value="<?php echo $model->kode; ?>" /><br><br>
-
-		<?php echo $model->label['nama']; ?><br>
-		<input type="text" name="nama" size="30" maxlength="25" value="<?php echo $model->nama; ?>"/><br><br>
-
-		<?php echo $model->label['harga']; ?><br>
-		<textarea name="harga"><?php echo $model->harga; ?></textarea><br><br>
-
-		<?php echo $model->label['stok']; ?><br>
-		<textarea name="stok"><?php echo $model->harga; ?></textarea><br><br>
-
-		<input type="submit" name="btnSubmit" value="Simpan" />
-		<input type="button" value="Batal" onclick="javascript:history.go(-1);" />
-	</form>
-
-</body>
-</html>
+<?php echo form_open('Petugas/update');?>
+	<?php $this->load->view($form)?>
+    	<!-- <button type="submit"> SIMPAN</button> -->
+<?php echo form_close(); ?>
